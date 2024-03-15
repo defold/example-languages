759781efba224867e2f0ba9e962cbd70cdcfdbf4
JCash <mwesterdahl76@gmail.com>
2024-03-15 16:06:36 +0100

    Rebuilt with latest sdk

--- decoder/src/decoder.zig
+++ decoder/src/decoder.zig
@@ -9,13 +9,13 @@ const c = @cImport({
 var g_ExtensionDesc: ?*c.dmExtensionDesc = null;
 
 fn Decode(L: ?*c.lua_State) callconv(.C) i32 {
-    var msglen : usize = 0;
-    var msg : [*c]const u8 = c.luaL_checklstring(L, 1, &msglen);
+    var msglen: usize = 0;
+    var msg: [*c]const u8 = c.luaL_checklstring(L, 1, &msglen);
 
-    const t : []u8 = std.heap.c_allocator.alloc(u8, msglen) catch "";
+    const t: []u8 = std.heap.c_allocator.alloc(u8, msglen) catch "";
     std.mem.copy(u8, t, msg[0..msglen]);
 
-    var i : usize = 0;
+    var i: usize = 0;
     while (i < msglen) : (i += 1) {
         t[i] -= 1;
     }
@@ -24,10 +24,7 @@ fn Decode(L: ?*c.lua_State) callconv(.C) i32 {
     return 1;
 }
 
-const Module_methods = [_]c.luaL_Reg{
-    .{.name = "decode", .func = &Decode},
-    .{.name = 0, .func = null}
-};
+const Module_methods = [_]c.luaL_Reg{ .{ .name = "decode", .func = &Decode }, .{ .name = 0, .func = null } };
 
 fn RegisterModule(L: ?*c.lua_State) void {
     c.luaL_register(L, "decoder", &Module_methods[0]);
@@ -35,25 +32,24 @@ fn RegisterModule(L: ?*c.lua_State) void {
 }
 
 fn Initialize(params: ?*c.dmExtensionParams) callconv(.C) c_int {
-    const L : ?*c.lua_State = params.?.m_L;
+    const L: ?*c.lua_State = params.?.m_L;
     RegisterModule(L);
-    return c.RESULT_OK;
+    return c.DM_EXTENSION_RESULT_OK;
 }
 
 fn Finalize(params: ?*c.dmExtensionParams) callconv(.C) c_int {
     _ = params; // unused
-    return c.RESULT_OK;
+    return c.DM_EXTENSION_RESULT_OK;
 }
 
 fn Update(params: ?*c.dmExtensionParams) callconv(.C) c_int {
     _ = params; // unused
-    return c.RESULT_OK;
+    return c.DM_EXTENSION_RESULT_OK;
 }
 
 export fn ExtensionZIG() callconv(.C) void {
     g_ExtensionDesc = std.heap.c_allocator.create(c.dmExtensionDesc) catch null;
-    c.dmExtensionRegister(g_ExtensionDesc, @bitSizeOf(c.dmExtensionDesc), "ExtensionZIG",
-                null, null, Initialize, Finalize, Update, null);
+    c.dmExtensionRegister(g_ExtensionDesc, @bitSizeOf(c.dmExtensionDesc), "ExtensionZIG", null, null, Initialize, Finalize, Update, null);
 }
 
 export const mod_init_func_ptr linksection("__DATA,__mod_init_func") = &ExtensionZIG; // note the & op
